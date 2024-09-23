1f3ec31a34be986d95aae2ab430e978216a61603
Lucasna28 <U2451146@rts-365.dk>
2024-09-23 13:08:25 +0200

    fix

--- src/tiles/updateTiles.ts
+++ src/tiles/updateTiles.ts
@@ -6,10 +6,10 @@ export default function updateTiles() {
 
   // Function to check if a position is within the tileOutline
   const isWithinTileOutline = (x: number, y: number) => {
-    const minX = this.player.x - 1500;
-    const maxX = this.player.x + 1500;
-    const minY = this.player.y - 1500;
-    const maxY = this.player.y + 1500;
+    const minX = this.player.x - 1;
+    const maxX = this.player.x + 1;
+    const minY = this.player.y - 1;
+    const maxY = this.player.y + 1;
     return x >= minX && x < maxX && y >= minY && y < maxY;
   };
 
