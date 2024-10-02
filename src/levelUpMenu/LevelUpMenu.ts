452cd5b28e002a9dfd718d5b84bd5399265e9d8b
Lucasna28 <U2451146@rts-365.dk>
2024-10-02 10:57:55 +0200

    regen+powerup

--- src/levelUpMenu/LevelUpMenu.ts
+++ src/levelUpMenu/LevelUpMenu.ts
@@ -3,11 +3,13 @@ import { PlayerWithStats } from '../player/PlayerStats';
 import MyGame from 'src/myGame/myGame';
 
 export default class LevelUpMenu extends Phaser.Scene {
-  private powerUpOptions!: any[];  // Store random power-ups
-  private powerUpContainers!: Phaser.GameObjects.Container[]; // Hold power-up containere
-  private background!: Phaser.GameObjects.Image; // Baggrund som billede
-  private gameScene!: MyGame; // Reference til MyGame scenen
-  private isMenuOpen: boolean = false; // Tjek om menuen allerede er åben
+  private powerUpOptions!: any[];
+  private powerUpContainers!: Phaser.GameObjects.Container[];
+  private background!: Phaser.GameObjects.Image;
+  private gameScene!: MyGame;
+  private isMenuOpen: boolean = false;
+  private powerUpInfoBox!: Phaser.GameObjects.Container; // Container til info-boksen
+  private powerUpInfoText!: Phaser.GameObjects.Text; // Tekstfelt til info-boksen
 
   constructor() {
     super({ key: 'LevelUpMenu' });
@@ -45,7 +47,23 @@ export default class LevelUpMenu extends Phaser.Scene {
       button.y += index * 80; // Plads mellem knapperne
     });
 
+    // Opret info-boksen
+    this.createPowerUpInfoBox();
+
     this.gameScene = this.scene.get('MyGame') as MyGame;
+  }
+
+  private createPowerUpInfoBox(): void {
+    const { width, height } = this.cameras.main;
+
+    this.powerUpInfoBox = this.add.container(width / 2, height - 100);
+    const box = this.add.rectangle(0, 0, 300, 100, 0x000000, 0.7).setOrigin(0.5);
+    this.powerUpInfoText = this.add.text(0, 0, '', { fontSize: '16px', color: '#fff', wordWrap: { width: 280 } })
+      .setOrigin(0.5)
+      .setAlign('center');
+
+    this.powerUpInfoBox.add([box, this.powerUpInfoText]);
+    this.powerUpInfoBox.setVisible(false); // Skjul info-boksen som standard
   }
 
   private createPowerUpButton(powerUp: any, index: number): Phaser.GameObjects.Container {
@@ -58,11 +76,13 @@ export default class LevelUpMenu extends Phaser.Scene {
         button.setFillStyle(0x5B99E2);
         button.setScale(1.05);
         this.addGlowEffect(button);
+        this.showPowerUpInfo(powerUp); // Vis info om power-up
       })
       .on('pointerout', () => {
         button.setFillStyle(0x4A90E2);
         button.setScale(1);
         this.removeGlowEffect(button);
+        this.powerUpInfoBox.setVisible(false); // Skjul info-boksen ved hover ud
       })
       .on('pointerdown', () => {
         button.disableInteractive();
@@ -84,49 +104,54 @@ export default class LevelUpMenu extends Phaser.Scene {
     return this.add.container(0, 0, [buttonShadow, button, image, text]);
   }
 
+  private showPowerUpInfo(powerUp: any): void {
+    // Opdater tekstfeltet med information om power-up
+    this.powerUpInfoText.setText(`Title: ${powerUp.title}\nValue: ${powerUp.value}`);
+    this.powerUpInfoBox.setVisible(true);
+  }
+
   private selectPowerUp(powerUp: any): void {
-    // Håndter den valgte power-up ved at tilføje værdien til spillerens stats
     switch (powerUp.imageKey) {
-        case 'powerUpDamage':
-            this.gameScene.player.stats.damage += powerUp.value;
-            break;
-        case 'powerUpSpeed':
-            this.gameScene.player.stats.speed += powerUp.value;
-            break;
-        case 'powerUpHealth':
-            this.gameScene.player.stats.health += powerUp.value;
-            break;
-        case 'powerUpCooldown':
-            this.gameScene.player.stats.dashCooldown += powerUp.value; // Reduktion af cooldown
-            break;
-        case 'powerUpFireRate':
-            this.gameScene.player.stats.fireRate += powerUp.value; // Øget skudhastighed
-            break;
-        case 'powerUpDashSpeed':
-            this.gameScene.player.stats.dashSpeed += powerUp.value;
-            break;
-        case 'powerUpDashDuration':
-            this.gameScene.player.stats.dashDuration += powerUp.value;
-            break;
-        case 'powerUpBulletSpeed':
-            // Hvis du har en bulletSpeed-stat i dine spillerstats, kan du tilføje her.
-            break;
-        default:
-            console.log('Ukendt power-up valgt');
+      case 'powerUpDamage':
+        this.gameScene.player.stats.damage += powerUp.value;
+        break;
+      case 'powerUpSpeed':
+        this.gameScene.player.stats.speed += powerUp.value;
+        break;
+      case 'powerUpHealth':
+        this.gameScene.player.stats.health += powerUp.value;
+        break;
+      case 'powerUpCooldown':
+        this.gameScene.player.stats.dashCooldown += powerUp.value;
+        break;
+      case 'powerUpFireRate':
+        this.gameScene.player.stats.fireRate += powerUp.value;
+        break;
+      case 'powerUpDashSpeed':
+        this.gameScene.player.stats.dashSpeed += powerUp.value;
+        break;
+      case 'powerUpDashDuration':
+        this.gameScene.player.stats.dashDuration += powerUp.value;
+        break;
+      case 'powerUpBulletSpeed':
+        break;
+      case 'powerUpMagnet':
+        this.gameScene.player.magnetRadius += powerUp.value;
+        break;
+      default:
+        console.log('Ukendt power-up valgt');
     }
 
-    console.log('Power-up valgt:', powerUp);
     this.gameScene.handlePowerUpSelection();
-}
+    this.powerUpInfoBox.setVisible(false); // Skjul info-boksen efter valg
+  }
 
-  // Sørg for at nulstille "isMenuOpen" når menuen lukkes
   shutdown(): void {
     console.log('LevelUpMenu shutdown');
-    this.isMenuOpen = false;  // Nulstil flagget, så menuen kan åbnes igen
+    this.isMenuOpen = false; // Nulstil flagget, så menuen kan åbnes igen
   }
 
   private addGlowEffect(button: Phaser.GameObjects.Rectangle): void {
-    // Glødeffekt på hover
     this.tweens.add({
       targets: button,
       alpha: 1,
@@ -136,7 +161,6 @@ export default class LevelUpMenu extends Phaser.Scene {
   }
 
   private removeGlowEffect(button: Phaser.GameObjects.Rectangle): void {
-    // Fjern glødeffekt
     this.tweens.add({
       targets: button,
       alpha: 0.9,
