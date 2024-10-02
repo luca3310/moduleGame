8e3c10e90e6987251efd36ae557cde964b586b08
Lucasna28 <U2451146@rts-365.dk>
2024-10-02 13:09:56 +0200

    wefowefojenf

--- src/myGame/preload.ts
+++ src/myGame/preload.ts
@@ -5,10 +5,7 @@ export function preload(this: Phaser.Scene): void {
     this.load.image('enemyWalk1', 'assets/Enemy/zombie_walk1.png');
     this.load.image('enemyWalk2', 'assets/Enemy/zombie_walk2.png');
 
-    this.load.image('tile1', 'assets/Tiled/tile_0000.png');
-    this.load.image('tile2', 'assets/Tiled/tile_0001.png');
-    this.load.image('tile3', 'assets/Tiled/tile_0002.png');
-
+    
     this.load.audio('ambience', 'assets/Music/Zombies.mp3');
     this.load.audio('zombieHit', 'assets/Sounds/Zombie/163447__under7dude__zombie-hit.wav');
     this.load.json('powerUps', 'assets/powerUps.json');
